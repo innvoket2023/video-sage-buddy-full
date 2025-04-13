959219703d3727b6589c83256450eafca7ff0c62
anshravalll <anshraval@gmail.com>
2025-04-13 23:55:38 +0530

    playing with langchain sqlalchemy chat history implementation

---
+++ backend/app/chat/chat.py
@@ -0,0 +1,12 @@
+from ... import config
+from langchain_core.messages import HumanMessage
+import os
+from dotenv import load_dotenv
+from langchain_community.chat_message_histories import SQLChatMessageHistory
+load_dotenv()
+# create sync sql message history by connection_string
+message_history = SQLChatMessageHistory(
+    session_id='foo', connection_string= os.getenv("SQLALCHEMY_DATABASE_URI")
+)
+message_history.add_message(HumanMessage("hello"))
+print(message_history.messages)
