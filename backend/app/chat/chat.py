36eb3b8d5dcef29d3a41385dd82b0cddd3527f2d
anshravalll <anshraval@gmail.com>
2025-04-20 23:55:55 +0530

    reorganized importing

--- backend/app/chat/chat.py
+++ backend/app/chat/chat.py
@@ -4,11 +4,11 @@ import uuid
 from dotenv import load_dotenv
 from langchain_community.chat_message_histories import SQLChatMessageHistory
 from flask import Blueprint, request, jsonify, current_app
+from langchain_google_genai import ChatGoogleGenerativeAI
+from sqlalchemy.exc import IntegrityError
 from app.middleware import jwt_required, admin_required
 from app.models import ChatSession
 from app.extensions import db
-from langchain_google_genai import ChatGoogleGenerativeAI
-from sqlalchemy.exc import IntegrityError
 
 class ChatUtil:
     def __init__(self):
